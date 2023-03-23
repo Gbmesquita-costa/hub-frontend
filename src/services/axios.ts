06705d1f15796554b8eeb82917a93f8a0a060946
gabriel <mesquitag313@gmail.com>
2023-03-22 23:08:36 -0300

    services

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,7 +4,7 @@ import { destroyCookie, parseCookies } from "nookies"
 const { "user_token": token } = parseCookies()
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "http://localhost:8080"
 })
 
 if (token) {
